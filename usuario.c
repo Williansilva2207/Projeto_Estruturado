089c635eca1869715c5a20b49c18690d2132a933
vinnianderson <128402558+vinnianderson@users.noreply.github.com>
2024-06-04 12:05:52 +0000

    Começo de algo

---
+++ usuario.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TAM_NOME 50
+#define TAM_LOGIN 20
+#define TAM_SENHA 20
+#define TAM_PLAYLIST 10
+#define TAM_MUSICA 100
+
+typedef struct {
+    int codigo;
+    char titulo[TAM_NOME];
+    char artista[TAM_NOME];
+}Musica;
+typedef struct {
+    int codigo;
+    char titulo[TAM_NOME];
+    int qtdMusicas;
+    Musica musicas[TAM_MUSICA];
+} Playlist;
+typedef struct {
+    int codigo;
+    char login[TAM_LOGIN];
+    char senha[TAM_SENHA];
+    int qtdPlaylist;
+    Playlist playlists [TAM_PLAYLIST];
+    int qtdPlaylistFav;
+    int listPlaylistFav[TAM_PLAYLIST];
+};
